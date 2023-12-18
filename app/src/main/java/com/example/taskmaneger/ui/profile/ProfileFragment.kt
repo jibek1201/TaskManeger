8cff0a38d08c3877a1e6505119113343bb9e75b0
zhibek <romanbekovazibek@gmail.com>
2023-12-18 16:50:28 +0600

    Initial commit

---
+++ app/src/main/java/com/example/taskmaneger/ui/profile/ProfileFragment.kt
@@ -0,0 +1,34 @@
+package com.example.taskmaneger.ui.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.taskmaneger.R
+import com.example.taskmaneger.databinding.FragmentNotificationsBinding
+
+
+class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentNotificationsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
